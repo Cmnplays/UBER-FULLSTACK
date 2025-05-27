b6a988e06fe1e330af14f7fb07989c8588d3eb47
Aaaaadddddiiii <chaurasiyarita20@gmail.com>
2025-05-27 13:21:04 +0545

    add credentials true in backend cors

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -4,7 +4,12 @@ import userRouter from "./routes/user.routes.js";
 import captianRouter from "./routes/captian.routes.js";
 import cookieParser from "cookie-parser";
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true
+  })
+);
 app.use(
   express.json({
     limit: "16kb"
